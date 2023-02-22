e494b7fc5e82f413a3ee46b771c30f15f5224dfd
knowak013 <kamila.nowak013@gmail.com>
2023-02-22 22:18:11 +0100

    Added  fade In/Out and switching views

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ const getDomElem = (id) => {
 };
 
 const conectHTMLElems = () => {
+  viewElems.mainContainer = getDomElem("mainContainer");
   viewElems.weatherInfo = getDomElem("weatherInfo");
   viewElems.searchInput = getDomElem("searchInput");
   viewElems.searchButton = getDomElem("searchButton");
+  viewElems.weatherSearchView = getDomElem("weatherSearchView");
   viewElems.weatherForecastView = getDomElem("weatherForecastView");
   viewElems.returnToSearchBtn = getDomElem("returnToSearchBtn");
   viewElems.weatherIcon = getDomElem("weatherIcon");
@@ -20,6 +22,10 @@ const conectHTMLElems = () => {
 const setupListeners = () => {
   viewElems.searchInput.addEventListener("keydown", onEnterSubmit);
   viewElems.searchButton.addEventListener("click", onClickSubmit);
+  viewElems.returnToSearchBtn.addEventListener(
+    "click",
+    onClickrReturnToSearchBtn
+  );
 };
 
 const initializeApp = () => {
@@ -28,12 +34,54 @@ const initializeApp = () => {
 };
 
 const onEnterSubmit = (event) => {
-  console.log(event.key);
-  if (event.key == "Enter") {
+  if (event.key === "Enter") {
+    fadeInOut();
+    viewElems.mainContainer.style.opacity = "0";
     let city = viewElems.searchInput.value;
     getWeatherByCity(city).then((data) => console.log(data));
+    setTimeout(() => {
+      switchView();
+      fadeInOut();
+    }, 500);
   }
 };
-const onClickSubmit = () => {};
+const onClickSubmit = () => {
+  fadeInOut();
+  let city = viewElems.searchInput.value;
+  getWeatherByCity(city).then((data) => console.log(data));
+  setTimeout(() => {
+    switchView();
+    fadeInOut();
+  }, 500);
+};
+
+const onClickrReturnToSearchBtn = () => {
+  fadeInOut();
+  setTimeout(() => {
+    switchView();
+    fadeInOut();
+  }, 500);
+};
+
+const fadeInOut = () => {
+  if (
+    viewElems.mainContainer.style.opacity === "1" ||
+    viewElems.mainContainer.style.opacity === ""
+  ) {
+    viewElems.mainContainer.style.opacity = "0";
+  } else {
+    viewElems.mainContainer.style.opacity = "1";
+  }
+};
+
+const switchView = () => {
+  if (viewElems.weatherSearchView.style.display !== "none") {
+    viewElems.weatherSearchView.style.display = "none";
+    viewElems.weatherForecastView.style.display = "flex";
+  } else {
+    viewElems.weatherSearchView.style.display = "flex";
+    viewElems.weatherForecastView.style.display = "none";
+  }
+};
 
 document.addEventListener("DOMContentLoaded", initializeApp);
