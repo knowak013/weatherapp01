e80c0dba9d5dc045148c21cde01eac0a4b48c256
knowak013 <kamila.nowak013@gmail.com>
2023-02-23 21:39:39 +0100

    Refactor

--- src/main.js
+++ src/main.js
@@ -1,97 +1,85 @@
 import { getWeatherByCity } from "./apiService.js";
+import { mapListToDomElements } from "./DOMActions.js";
 const viewElems = {};
 
-const getDomElem = (id) => {
-  return document.getElementById(id);
-};
-
-const conectHTMLElems = () => {
-  viewElems.mainContainer = getDomElem("mainContainer");
-  viewElems.weatherInfo = getDomElem("weatherInfo");
-  viewElems.searchInput = getDomElem("searchInput");
-  viewElems.searchButton = getDomElem("searchButton");
-  viewElems.weatherSearchView = getDomElem("weatherSearchView");
-  viewElems.weatherForecastView = getDomElem("weatherForecastView");
-  viewElems.returnToSearchBtn = getDomElem("returnToSearchBtn");
-  viewElems.weatherCurrentTemp = getDomElem("weatherCurrentTemp");
-  viewElems.weatherCity = getDomElem("weatherCity");
-  viewElems.fellsLike = getDomElem("fellsLike");
-  viewElems.wind = getDomElem("wind");
-  viewElems.cloud = getDomElem("cloud");
-};
-
-const setupListeners = () => {
-  viewElems.searchInput.addEventListener("keydown", onEnterSubmit);
-  viewElems.searchButton.addEventListener("click", onClickSubmit);
-  viewElems.returnToSearchBtn.addEventListener(
-    "click",
-    onClickrReturnToSearchBtn
-  );
-};
-
-const initializeApp = () => {
-  conectHTMLElems();
-  setupListeners();
-};
-
-const onEnterSubmit = (event) => {
-  if (event.key === "Enter") {
-    fadeInOut();
-    viewElems.mainContainer.style.opacity = "0";
-    let city = viewElems.searchInput.value;
-    getWeatherByCity(city).then((data) => displayWeatherData(data));
+class weatherApp {
+  constructor() {
+    this.viewElems = {};
+    this.conncectDOMElements();
+    this.setupListeners();
+  }
+  conncectDOMElements = () => {
+    const listOfId = Array.from(document.querySelectorAll("[id]")).map(
+      (elem) => elem.id
+    );
+    this.viewElems = mapListToDomElements(listOfId);
+  };
+  setupListeners = () => {
+    this.viewElems.searchInput.addEventListener("keydown", this.onEnterSubmit);
+    this.viewElems.searchButton.addEventListener("click", this.onClickSubmit);
+    this.viewElems.returnToSearchBtn.addEventListener(
+      "click",
+      this.onClickrReturnToSearchBtn
+    );
+  };
+  onEnterSubmit = (event) => {
+    if (event.key === "Enter") {
+      this.fadeInOut();
+      this.viewElems.mainContainer.style.opacity = "0";
+      let city = this.viewElems.searchInput.value;
+      getWeatherByCity(city).then((data) => displayWeatherData(data));
+      setTimeout(() => {
+        this.switchView();
+        this.fadeInOut();
+      }, 500);
+    }
+  };
+  onClickSubmit = () => {
+    this.fadeInOut();
+    let city = this.viewElems.searchInput.value;
+    getWeatherByCity(city).then((data) => this.displayWeatherData(data));
     setTimeout(() => {
-      switchView();
-      fadeInOut();
+      this.switchView();
+      this.fadeInOut();
     }, 500);
-  }
-};
-const onClickSubmit = () => {
-  fadeInOut();
-  let city = viewElems.searchInput.value;
-  getWeatherByCity(city).then((data) => displayWeatherData(data));
-  setTimeout(() => {
-    switchView();
-    fadeInOut();
-  }, 500);
-};
+  };
 
-const onClickrReturnToSearchBtn = () => {
-  fadeInOut();
-  setTimeout(() => {
-    switchView();
-    fadeInOut();
-  }, 500);
-};
+  onClickrReturnToSearchBtn = () => {
+    this.fadeInOut();
+    setTimeout(() => {
+      this.switchView();
+      this.fadeInOut();
+    }, 500);
+  };
 
-const fadeInOut = () => {
-  if (
-    viewElems.mainContainer.style.opacity === "1" ||
-    viewElems.mainContainer.style.opacity === ""
-  ) {
-    viewElems.mainContainer.style.opacity = "0";
-  } else {
-    viewElems.mainContainer.style.opacity = "1";
-  }
-};
+  fadeInOut = () => {
+    if (
+      this.viewElems.mainContainer.style.opacity === "1" ||
+      this.viewElems.mainContainer.style.opacity === ""
+    ) {
+      this.viewElems.mainContainer.style.opacity = "0";
+    } else {
+      this.viewElems.mainContainer.style.opacity = "1";
+    }
+  };
 
-const switchView = () => {
-  if (viewElems.weatherSearchView.style.display !== "none") {
-    viewElems.weatherSearchView.style.display = "none";
-    viewElems.weatherForecastView.style.display = "flex";
-  } else {
-    viewElems.weatherSearchView.style.display = "flex";
-    viewElems.weatherForecastView.style.display = "none";
-  }
-};
+  switchView = () => {
+    if (this.viewElems.weatherSearchView.style.display !== "none") {
+      this.viewElems.weatherSearchView.style.display = "none";
+      this.viewElems.weatherForecastView.style.display = "flex";
+    } else {
+      this.viewElems.weatherSearchView.style.display = "flex";
+      this.viewElems.weatherForecastView.style.display = "none";
+    }
+  };
 
-const displayWeatherData = (data) => {
-  console.log(data);
-  viewElems.weatherCity.innerText = data.location.name;
-  viewElems.weatherCurrentTemp.innerText = `Temperature: ${data.current.temp_c}°C`;
-  viewElems.fellsLike.innerText = `Sensed temperature: ${data.current.feelslike_c}°C`;
-  viewElems.wind.innerText = `Wind: ${data.current.wind_kph} kph`;
-  viewElems.cloud.innerText = `Cloudiness: ${data.current.cloud} %`;
-};
-
-document.addEventListener("DOMContentLoaded", initializeApp);
+  displayWeatherData = (data) => {
+    console.log(data);
+    this.viewElems.weatherCity.innerText = data.location.name;
+    this.viewElems.weatherCurrentTemp.innerText = `Temperature: ${data.current.temp_c}°C`;
+    this.viewElems.fellsLike.innerText = `Sensed temperature: ${data.current.feelslike_c}°C`;
+    this.viewElems.wind.innerText = `Wind: ${data.current.wind_kph} kph`;
+    this.viewElems.cloud.innerText = `Cloudiness: ${data.current.cloud} %`;
+  };
+}
+document.addEventListener("DOMContentLoaded", new weatherApp());
